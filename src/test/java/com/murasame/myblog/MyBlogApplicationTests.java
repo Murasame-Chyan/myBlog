d248bd74ae92ff4b5fec1cb2b12ee83ebb29b959
Murasame-Chyan <3565066138@qq.com>
2025-12-03 15:28:24 +0800

    初始项目 Default Project

---
+++ src/test/java/com/murasame/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murasame.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
